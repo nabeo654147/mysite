243811fdb7bafb82610782d1f7daa6c83f7e7f4b
nabeo654147 <nabehiko616@gmail.com>
2021-12-27 00:16:26 +0900

    Headerコンポーネントの作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import { VFC } from 'react';
+import Navbar from './Navbar';
+
+const Header: VFC<Partial<HTMLHeadElement>> = () => {
+  return (
+    <header className='flex justify-between flex-wrap'>
+      <Image src='/img/logo.png' height={80} width={140} />
+      <Navbar titles={['ABOUT', 'SKILLS', 'VALUES', 'FUTURE']}></Navbar>
+    </header>
+  );
+};
+
+export default Header;
