258c193f7a46b3705d6bbed7c603219897b3b594
nabeo654147 <nabehiko616@gmail.com>
2021-12-28 17:17:23 +0900

    MainのHead作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Footer from '@/components/Footer';
 import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,15 @@ const Home: NextPage = () => {
         <link rel='icon' href='/img/sommelier.png' />
       </Head>
       <Header />
+      <div className='bg-[#6bc2c3] flex flex-wrap justify-between h-210 text-[#fff]'>
+        <div className='flex flex-col my-auto ml-[200px]'>
+          <h1 className='text-6xl pb-4'>Nabelog</h1>
+          <p className='text-2xl text-[#d9f1eb]'>サブタイトル</p>
+        </div>
+        <span className='flex justify-end pb-6 pt-10 pr-10'>
+          <Image src='/img/hero.png' height={500} width={570}></Image>
+        </span>
+      </div>
 
       <main></main>
 
