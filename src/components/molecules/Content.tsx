3e1247b6e9846ae9dbf4cd70dc3f6dc3f3681578
nabeo654147 <nabehiko616@gmail.com>
2022-01-03 16:01:31 +0900

    Contentコンポーネントへ渡すprops追加

--- src/components/molecules/Content.tsx
+++ src/components/molecules/Content.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
-import { VFC } from 'react';
+import React, { VFC } from 'react';
 
 export type ContentProps = {
   contents: {
     image: string;
+    imageHeight: number;
+    imageWidth: number;
     title: string;
+    titleCSS?: string;
     description: string;
+    partition?: boolean;
+    valueTitel?: boolean;
   }[];
 };
 
@@ -14,11 +19,24 @@ const Content: VFC<ContentProps> = ({ contents }) => {
     <div className='flex justify-center'>
       {contents.map((content) => {
         return (
-          <div className='flex flex-col items-center max-w-[300px] m-6 pt-6' key={content.title}>
-            <Image src={`/img/${content.image}`} height={150} width={200} />
-            <h3 className='text-2xl text-[#008c8d] font-semibold py-4'>{content.title}</h3>
-            <p className='text-[#384359]'>{content.description}</p>
-          </div>
+          <React.Fragment key={content.title}>
+            <div className='flex flex-col items-center max-w-[300px] m-6 pt-6'>
+              <Image
+                src={`/img/${content.image}`}
+                height={content.imageHeight}
+                width={content.imageWidth}
+              />
+              {content.valueTitel === true ? (
+                <h3 className='text-[#384359] font-semibold py-4 '>{content.title}</h3>
+              ) : (
+                <h3 className='text-2xl text-[#008c8d] font-semibold py-4'>{content.title}</h3>
+              )}
+              <p className='text-[#384359] text-md'>{content.description}</p>
+            </div>
+            {content.partition === true && (
+              <div className='w-0.5 h-[430px] bg-[#c5eaea] my-auto'></div>
+            )}
+          </React.Fragment>
         );
       })}
     </div>
--- src/textData/content.ts
+++ src/textData/content.ts
@@ -4,21 +4,61 @@ export const contentAbout: ContentProps = {
   contents: [
     {
       image: 'about-1.png',
+      imageHeight: 150,
+      imageWidth: 200,
       title: '趣味',
       description:
         'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト',
     },
     {
       image: 'about-2.png',
+      imageHeight: 150,
+      imageWidth: 200,
       title: '好きな食べ物',
       description:
         'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト',
     },
     {
       image: 'about-3.png',
+      imageHeight: 150,
+      imageWidth: 200,
       title: '性格',
       description:
         'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト',
+    },
+  ],
+};
+
+export const contentValues: ContentProps = {
+  contents: [
+    {
+      image: 'value1.png',
+      imageHeight: 200,
+      imageWidth: 200,
+      title: '価値観01',
+      description:
+        'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト',
+      partition: true,
+      valueTitel: true,
+    },
+    {
+      image: 'value2.png',
+      imageHeight: 200,
+      imageWidth: 200,
+      title: '価値観02',
+      description:
+        'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト',
+      partition: true,
+      valueTitel: true,
+    },
+    {
+      image: 'value3.png',
+      imageHeight: 200,
+      imageWidth: 200,
+      title: '価値観03',
+      description:
+        'テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト',
+      valueTitel: true,
     },
   ],
 };
