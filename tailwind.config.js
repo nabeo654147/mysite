4eadf14910aabc3d31399c4d6dab29a9af1736c2
nabeo654147 <nabehiko616@gmail.com>
2022-01-05 20:15:26 +0900

    共通の色コードをtailwind.config.jsへ追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         skills: `url(/img/bg-skills.png)`,
         future: `url(/img/bg-future.png)`,
       }),
+      colors: {
+        'font-dark': '#384359',
+        'font-green': '#008c8d',
+      },
     },
   },
   plugins: [],
