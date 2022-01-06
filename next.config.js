ca2bca36ce9a0bd66875cd997bd3ff0f33a6c8d4
nabeo654147 <nabehiko616@gmail.com>
2022-01-06 18:17:01 +0900

    next.config.jsの画像関係の設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    deviceSizes: [340, 640, 768, 1024, 1280, 1440, 1980],
+    domains: ['fwywd.com'],
+  },
 };
