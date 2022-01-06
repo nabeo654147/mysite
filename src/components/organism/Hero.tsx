33db99411843bed66e08c5a0bcb0ca4fa5305422
nabeo654147 <nabehiko616@gmail.com>
2022-01-06 21:17:57 +0900

    styleの調整

--- src/components/organism/Hero.tsx
+++ src/components/organism/Hero.tsx
@@ -9,10 +9,10 @@ type Props = {
 const Hero: VFC<Props> = ({ topTitle, subTitle }) => {
   return (
     <section className=' bg-[#6bc2c3]'>
-      <div className='grid grid-cols-2 flex-wrap max-w-[90%] ml-auto text-[#fff]'>
+      <div className='grid flex-wrap grid-cols-2 ml-auto max-w-[90%] text-[#fff]'>
         <div className='flex flex-col my-auto'>
           <h1 className='text-6xl tracking-widest'>{topTitle}</h1>
-          <p className='font-semibold mt-4 text-2xl text-[#d9f1eb]'>{subTitle}</p>
+          <p className='mt-4 text-2xl font-semibold text-[#d9f1eb]'>{subTitle}</p>
         </div>
         <span className='flex justify-end pt-10 pr-10 pb-6'>
           <Image src='/img/hero.png' alt={'hero'} height={498} width={585}></Image>
