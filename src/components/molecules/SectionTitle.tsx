c08e59c8f6fc183d6cd096d1db6c9220979fba45
nabeo654147 <nabehiko616@gmail.com>
2022-01-06 18:18:30 +0900

    Scroll Behavior機能の追加

--- src/components/molecules/SectionTitle.tsx
+++ src/components/molecules/SectionTitle.tsx
@@ -9,7 +9,9 @@ type Props = {
 const SectionTitle: VFC<Props> = ({ title, subTitle, justify = 'center' }) => {
   return (
     <div className={`flex w-full justify-${justify}`}>
-      <h2 className='mr-4 text-3xl font-semibold text-font-dark'>{title}</h2>
+      <h2 className='mr-4 text-3xl font-semibold text-font-dark' id={subTitle}>
+        {title}
+      </h2>
       <p className='flex items-center text-xl font-normal text-font-green'>{subTitle}</p>
     </div>
   );
