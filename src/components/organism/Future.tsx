73eb52a00fc42f52a6427c09aaf91279cbbfbd16
nabeo654147 <nabehiko616@gmail.com>
2022-01-07 12:51:12 +0900

    styleの編集

--- src/components/organism/Future.tsx
+++ src/components/organism/Future.tsx
@@ -5,21 +5,15 @@ import SectionTitle from '../molecules/SectionTitle';
 const Future: VFC = () => {
   return (
     <section>
-      <div className='flex justify-center p-10 mt-[-315px]'>
+      <div className='flex justify-center p-10 mt-[-315px] bg-[#f1f7f8]'>
         <div className='pr-7 my-auto w-4/5 max-w-[550px]'>
           <SectionTitle title={'3年後にやりたいこと'} subTitle={'FUTURE'} justify={'start'} />
-          <p className='py-8 text-sm'>
+          <p className='py-8 text-sm leading-7'>
             テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
           </p>
         </div>
         <div className='my-auto bg-[#f1f7f8]'>
-          <Image
-            src='/img/future.png'
-            alt='future'
-            height={230}
-            width={450}
-            className='bg-[#f1f7f8]'
-          />
+          <Image src='/img/future.png' alt='future' height={251} width={480} />
         </div>
       </div>
     </section>
