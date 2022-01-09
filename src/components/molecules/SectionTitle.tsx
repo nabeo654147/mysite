e7508672c9d897b7b3089c34a42f11bf4e35623b
nabeo654147 <nabehiko616@gmail.com>
2022-01-09 20:41:43 +0900

    SectionTitle修正

--- src/components/molecules/SectionTitle.tsx
+++ src/components/molecules/SectionTitle.tsx
@@ -3,16 +3,16 @@ import { VFC } from 'react';
 type Props = {
   title: string;
   subTitle: string;
-  justify?: 'center' | 'start';
+  justify?: 'start' | 'center' | 'end';
 };
 
 const SectionTitle: VFC<Props> = ({ title, subTitle, justify = 'center' }) => {
   return (
-    <div className={`flex items-center flex-col sm:flex-row mx-auto justify-${justify}`}>
-      <h2 className='mr-4 text-2xl font-semibold text-font-dark sm:text-3xl' id={subTitle}>
+    <div className={`flex items-center flex-col md:flex-row mx-auto justify-${justify}`}>
+      <h2 className='text-2xl font-semibold text-font-dark sm:mr-4 sm:text-3xl' id={subTitle}>
         {title}
       </h2>
-      <h3 className='flex items-center text-xl font-normal text-font-green'>{subTitle}</h3>
+      <h3 className='flex items-center font-normal text-font-green sm:text-xl'>{subTitle}</h3>
     </div>
   );
 };
