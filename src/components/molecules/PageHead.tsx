462593678aad1229fd158e6f29c909bf7d1e8eff
nabeo654147 <nabehiko616@gmail.com>
2022-01-20 20:08:56 +0900

    OGP画像の追加&設定

--- src/components/molecules/PageHead.tsx
+++ src/components/molecules/PageHead.tsx
@@ -6,18 +6,21 @@ type Props = {
   description: string;
 };
 
-const PageHead: VFC<Props> = ({ title = 'Nabelog', description = 'Nabeoの自己紹介ページです' }) => {
+const PageHead: VFC<Props> = ({
+  title = 'Nabelog | Nabeoの自己紹介ページです',
+  description = 'Nabeoの自己紹介ページです',
+}) => {
   return (
     <Head>
       <title>{title}</title>
       <meta charSet='utf-8' />
       <meta property='description' content={description} />
-      <meta name='twitter:card' content='summary' />
+      <meta name='twitter:card' content='summary_large_image' />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
       <meta property='og:type' content='website' />
       <meta property='og:url' content={'https://fwywd-mysite.vercel.app/'} />
-      <meta property='og:image' content='/img/favicon.png' />
+      <meta property='og:image' content='https://fwywd-mysite.vercel.app/img/ogp.png' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta name='google-site-verification' content='19voYQSFkBtLthKoUHh0IJPMUEU9gnk-n4-bGXNmw3o' />
       <link rel='icon' href='/img/favicon.png' />
