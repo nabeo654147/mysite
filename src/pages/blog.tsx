5d1317df9cdb732ff9ececfdde8b928e509633fd
nabeo654147 <86973341+nabeo654147@users.noreply.github.com>
2022-02-13 22:12:46 +0900

    blogページ作成

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,18 +1,35 @@
+import { Client } from '@notionhq/client';
+import { GetStaticProps } from 'next';
 import { VFC } from 'react';
 import BlogHero from '@/components/organism/BlogHero';
 import BlogMain from '@/components/organism/BlogMain';
 import BlogPageHeader from '@/components/organism/BlogPageHeader';
 import PageFooter from '@/components/organism/PageFooter';
+// import type { NotionProperty } from '@types/notiondata';
+import { getNotionData } from 'lib/getNotionData';
 
-const BlogPage: VFC = () => {
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+const BlogPage: VFC<any> = ({ posts }) => {
   return (
     <>
       <BlogPageHeader />
       <BlogHero topTitle='Nabelog' subTitle='~気ままなブログ~' />
-      <BlogMain />
+      <BlogMain posts={posts} />
       <PageFooter />
     </>
   );
 };
 
 export default BlogPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const database = await getNotionData(process.env.NOTION_DATABASE_ID);
+
+  return {
+    props: {
+      posts: database,
+    },
+  };
+};
